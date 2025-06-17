667fe960f6528fda764f11cd8e531976110985b7
Kevin Munoz <kevin.munozgracia@ucr.ac.cr>
2025-06-16 19:58:08 -0600

    Se agrega el metodo insertar ultimo y primero en nodo.c

--- nodo.c
+++ nodo.c
@@ -24,3 +24,53 @@ int getTam(){
     
     return cont+1;
 }
+
+
+void insertarUltimo(struct Persona p) {
+    if (primero == NULL) {
+        primero = malloc(sizeof(struct Nodo));
+        primero->p = malloc(sizeof(struct Persona));
+        *(primero->p)      = p;
+        primero->anterior   = NULL;
+        primero->siguiente  = NULL;
+        
+        
+        // es valido?
+        ultimo = primero;
+    } else {
+        struct Nodo* actual = primero;
+        while (actual->siguiente != NULL) {
+            actual = actual->siguiente;
+        }
+        actual->siguiente = malloc(sizeof(struct Nodo));
+        struct Nodo* siguiente = actual->siguiente;
+        siguiente->p = malloc(sizeof(struct Persona));
+        *(siguiente->p) = p;
+        siguiente->anterior = actual;
+        siguiente->siguiente = NULL;
+        ultimo = siguiente;
+    }
+}
+
+void insertarPrimero(struct Persona p){
+    
+    if(primero == NULL){
+        primero = malloc(sizeof(struct Nodo));
+        primero->p = malloc(sizeof(struct Persona));
+        *(primero->p) = p;
+        primero->siguiente = NULL;
+        primero->anterior= NULL;
+        ultimo = primero;
+    }
+    
+    else{
+        struct Nodo* actual = malloc(sizeof(struct Nodo));
+        actual->p = malloc(sizeof(struct Persona));
+        *(actual->p) = p;
+        actual->siguiente = primero;
+        actual->anterior = NULL;
+        primero->anterior = actual;
+        primero = actual;
+    }
+    
+}
