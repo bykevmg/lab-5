c22099d1e6996f3121dc458c5f0a2a2b1f9f5153
Kevin Munoz <kevin.munozgracia@ucr.ac.cr>
2025-06-16 20:00:56 -0600

    Se agregan los metodos buscar por id, elimanr por id e insertar en n pos

--- nodo.c
+++ nodo.c
@@ -115,3 +115,144 @@ void imprimerFinAInicio(){
     
     
 }
+
+
+void buscaPorId(int idS){
+ 
+ 
+    bool encontrado = false;
+ 
+    if(primero == NULL){
+        printf("Lista vacia");
+    }
+    
+    struct Nodo* actual = primero;
+    
+    while(actual->siguiente != NULL){
+        
+        if(actual->p->id == idS){
+            printf("Persona encontrado: \n");
+            imprimirPersona( *(actual->p));
+            encontrado = true;
+        }
+        
+        actual = actual->siguiente;
+    }
+ 
+    if(encontrado == false){
+        printf("Persona no encontrada con id: %d\n", idS);
+    }
+
+
+ 
+    
+}
+
+
+
+
+void eliminaPorId(int idS){
+    
+    bool eliminado = false;
+    
+    
+    
+    if(primero == NULL){
+        printf("No hay nada que eliminar");
+    }
+    
+    if (primero->p->id == idS && primero->siguiente == NULL) {
+        free(primero->p);
+        free(primero);
+        primero = NULL;
+        ultimo  = NULL;
+        printf("Persona eliminada con id: %d\n", idS);
+        return;
+    }
+    
+    if (primero->p->id == idS) {
+        struct Nodo* actual = primero;
+        primero = primero->siguiente;
+        primero->anterior = NULL;
+        free(actual->p);
+        free(actual);
+        printf("Persona eliminada con id: %d\n", idS);
+        return;
+    }
+
+    
+
+    struct Nodo* actual = primero->siguiente;
+    while (actual != NULL) {
+        if (actual->p->id == idS) {
+         
+            if (actual->siguiente == NULL) {
+                ultimo = actual->anterior;
+                ultimo->siguiente = NULL;
+            } else {
+                
+                actual->anterior->siguiente = actual->siguiente;
+                
+                actual->siguiente->anterior = actual->anterior;
+            }
+            free(actual->p);
+            free(actual);
+            printf("Persona eliminada con id: %d\n", idS);
+            return;
+        }
+        actual = actual->siguiente;
+    }
+    
+    if(eliminado == false){
+        printf("Persona no encontrada con id: %d\n", idS);
+    }
+    
+    
+}
+
+void insertarEnPosN(int pos, struct Persona p){
+    
+
+    
+    struct Nodo* nuevo = malloc(sizeof(struct Nodo));
+    nuevo->p = malloc(sizeof(struct Persona));
+    *(nuevo->p) = p;
+    nuevo->siguiente = NULL;
+    nuevo->anterior = NULL;
+
+
+    if (pos < 1 || pos > getTam()) {
+        printf("Esa pos no es valida el tam es de %d\n", getTam());
+        return;
+    }
+
+    if (pos == 1) {
+        if (primero != NULL) {
+            primero->anterior = nuevo;
+            nuevo->siguiente  = primero;
+        } else {
+            ultimo = nuevo;
+        }
+        primero = nuevo;
+        return;
+    }
+
+    if (pos == getTam()) {
+        ultimo->siguiente = nuevo;
+        nuevo->anterior   = ultimo;
+        ultimo            = nuevo;
+        return;
+    }
+
+    int cont = 1;
+    struct Nodo* actual = primero;
+    while (cont < pos - 1) {
+        actual = actual->siguiente;
+        cont++;
+    }
+
+    nuevo->siguiente                = actual->siguiente;
+    nuevo->anterior                 = actual;
+    actual->siguiente->anterior     = nuevo;
+    actual->siguiente               = nuevo;
+}
